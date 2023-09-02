390ca01143577e9b0e010b8c4e59da993a46abaa
Jasu3k <126839512+Jasu3k@users.noreply.github.com>
2023-09-01 21:28:19 -0400

    Made compatible with relative paths

--- src/RookPiece.java
+++ src/RookPiece.java
@@ -3,17 +3,39 @@ import javafx.scene.image.ImageView;
 
 import java.awt.*;
 
+import java.io.InputStream;
+
 
 public class RookPiece extends ImageView{
     private Color color;
 
     public RookPiece(Color color, double size){
-        this.color = color;
+        if (color == Color.WHITE) {
+            // Define the relative path to the white pawn image
+            String relativePath = "assets/White/whiteRook.png";
 
-        if(color == Color.WHITE){
-            setImage(new Image("C:\\Users\\jassa\\OneDrive\\Desktop\\Jasu\\JasuProjects\\JavaChess\\ChessSprites\\White\\whiteRook.png"));
-        } else if(color == Color.BLACK){
-            setImage(new Image("C:\\Users\\jassa\\OneDrive\\Desktop\\Jasu\\JasuProjects\\JavaChess\\ChessSprites\\Black\\blackRook.png"));
+            // Load the image using a class loader
+            InputStream inputStream = getClass().getClassLoader().getResourceAsStream(relativePath);
+            if (inputStream != null) {
+                Image image = new Image(inputStream);
+                setImage(image);
+            } else {
+                // Handle the case where the resource could not be found
+                System.err.println("Image not found: " + relativePath);
+            }
+        } else if (color == Color.BLACK) {
+            // Define the relative path to the black pawn image
+            String relativePath = "assets/Black/blackRook.png";
+
+            // Load the image using a class loader
+            InputStream inputStream = getClass().getClassLoader().getResourceAsStream(relativePath);
+            if (inputStream != null) {
+                Image image = new Image(inputStream);
+                setImage(image);
+            } else {
+                // Handle the case where the resource could not be found
+                System.err.println("Image not found: " + relativePath);
+            }
         }
         setFitWidth(size);
         setFitHeight(size);
